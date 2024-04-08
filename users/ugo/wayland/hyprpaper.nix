0d51290d84e579981d13d5b69e22c6626aaca414
Ugo Proietti <ugo.pr@protonmail.com>
2024-04-08 12:36:55 +0200

    hyprpaper working

--- users/ugo/wayland/hyprpaper.nix
+++ users/ugo/wayland/hyprpaper.nix
@@ -1,11 +1,11 @@
-{inputs, ...}:
+{inputs, config, ...}:
 
 {
   imports = [ inputs.hyprpaper.homeManagerModules.default ];
   services.hyprpaper = {
     enable = true;
     preloads = [ "~/Documents/nixos-configuration/users/ugo/wayland/wallpaper.png" ];
-    wallpapers = [ "eDP-1, ~/Documents/nixos-configuration/users/ugo/wayland/wallpaper.png" ];
+    wallpapers = [ "eDP-1,~/Documents/nixos-configuration/users/ugo/wayland/wallpaper.png" ];
     splash = false;
     ipc = false;
   };
