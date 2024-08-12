024d496d59b050ea0bfb69f0e6116c60b3d18858
Ugo Proietti <ugo.pr@protonmail.com>
2024-08-12 08:44:54 +0200

    dual screen aux presses

--- users/ugo/wayland/hyprpaper/hyprpaper.nix
+++ users/ugo/wayland/hyprpaper/hyprpaper.nix
@@ -6,7 +6,7 @@
     enable = true;
     settings = {
       preload = [ "${./wallpaper.jpg}" ];
-      wallpaper = [ "eDP-1,${./wallpaper.jpg}" "HDMI-A-1,${./wallpaper.jpg}" ];
+      wallpaper = [ "eDP-1,${./wallpaper.jpg}" "HDMI-A-1,${./wallpaper.jpg}" "DP-3,${./wallpaper.jpg}" "DP-4,${./wallpaper.jpg}" ];
       splash = false;
       ipc = true;
     };
