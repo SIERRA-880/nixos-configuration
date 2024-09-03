abfffc5e053af40d381195d94eb62915e242b869
Ugo Proietti <ugo.pr@protonmail.com>
2024-09-03 18:07:47 +0200

    I changed system.stateVersion :omg:

--- system/configuration.nix
+++ system/configuration.nix
@@ -156,13 +156,13 @@
 
   nix.gc = {
     automatic = true;  # Enable the automatic garbage collector
-    dates = "03:15";   # When to run the garbage collector
-    options = "--delete-older-than 7d";    # Arguments to pass to nix-collect-garbage
+    # dates = "03:15";   # When to run the garbage collector
+    options = "--delete-older-than 28d";    # Arguments to pass to nix-collect-garbage
   };
   nix.optimise = {
     automatic = true;
-    dates = [ "03:15" ];
+    # dates = [ "03:15" ];
   };
 
-  system.stateVersion = "23.11";
+  system.stateVersion = "24.05";
 }
