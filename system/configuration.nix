c2a70822b5b5698fb4ba5c27f6fb5addbb2d6820
Ugo Proietti <ugo.pr@protonmail.com>
2024-09-25 20:13:04 +0200

    Update

--- system/configuration.nix
+++ system/configuration.nix
@@ -165,12 +165,10 @@
 
   nix.gc = {
     automatic = true;  # Enable the automatic garbage collector
-    # dates = "03:15";   # When to run the garbage collector
-    options = "--delete-older-than 28d";    # Arguments to pass to nix-collect-garbage
+    options = "--delete-older-than 60d";    # Arguments to pass to nix-collect-garbage
   };
   nix.optimise = {
     automatic = true;
-    # dates = [ "03:15" ];
   };
 
   system.stateVersion = "24.05";
